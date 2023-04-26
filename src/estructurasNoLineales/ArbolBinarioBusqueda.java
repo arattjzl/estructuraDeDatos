984e34dee8ba4fff068a267d0653cc94f2a0014b
aratt <aratt.jzl@gmail.com>
2023-04-25 22:26:02 -0600

    Se agrego los comentarios de documentacion a la clase de arbol binario de busqueda

--- src/estructurasNoLineales/ArbolBinarioBusqueda.java
+++ src/estructurasNoLineales/ArbolBinarioBusqueda.java
@@ -7,6 +7,10 @@ import utils.commons.Comparador;
 
 public class ArbolBinarioBusqueda extends ArbolBinario{
 
+    /**
+     * Se crea un árbol desde la raíz hasta las hojas.
+     * @param subraiz Es la raíz en la que iniciará el árbol.
+     */
     @Override
     protected void crearArbol(NodoDoble subraiz) {
         SalidaPorDefecto.terminal("¿El nodo " + subraiz.getInfo() + " tiene hijo izquierdo?  \n");
@@ -33,6 +37,11 @@ public class ArbolBinarioBusqueda extends ArbolBinario{
         }
     }
 
+    /**
+     * Agregará nodos al árbol, aún cuando no exista un árbol.
+     * @param info Información que se agregará al árbol.
+     * @return Regresa true si se pudo agregar y false si no.
+     */
     public boolean agregar(Object info){
         if(raiz == null){
             NodoDoble nuevoNodo = new NodoDoble(info);
@@ -47,6 +56,12 @@ public class ArbolBinarioBusqueda extends ArbolBinario{
         }
     }
 
+    /**
+     * Agregará nodos al árbol, aún cuando no exista árbol.
+     * @param subraiz Sub raíz en la que se encuentran del árbol.
+     * @param info Información que se agregará.
+     * @return Regresa true si se pudo agregar y false si no.
+     */
     private boolean agregar(NodoDoble subraiz, Object info){
         if((int) Comparador.comparar(info, subraiz.getInfo()) < 0){
             if(subraiz.getApuntadorAIzquierda() != null){
@@ -77,10 +92,21 @@ public class ArbolBinarioBusqueda extends ArbolBinario{
         }
     }
 
+    /**
+     * Busca el objeto en el árbol.
+     * @param info Información por buscar.
+     * @return Regresa el objeto si se encontró en el árbol y null si no.
+     */
     public Object buscar(Object info){
         return buscar(raiz, info);
     }
 
+    /**
+     * Busca el objeto en el árbol.
+     * @param subraiz Sub raíz del árbol en la que se encuentra.
+     * @param info Información por buscar.
+     * @return Regresa el objeto si se encontró y null si no.
+     */
     private Object buscar(NodoDoble subraiz, Object info){
         if(subraiz != null){
             if((int) Comparador.comparar(info, subraiz.getInfo()) < 0){
