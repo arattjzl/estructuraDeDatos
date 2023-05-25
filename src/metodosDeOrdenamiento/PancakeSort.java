ca3b1bb62c83e5d7ed3202d88b592b9f4bee60a3
aratt <aratt.jzl@gmail.com>
2023-05-24 21:10:00 -0600

    se inicio con pancake sort

--- src/metodosDeOrdenamiento/PancakeSort.java
+++ src/metodosDeOrdenamiento/PancakeSort.java
@@ -1,5 +1,53 @@
 package metodosDeOrdenamiento;
 
+import entradasalida.SalidaPorDefecto;
+import estructurasLineales.ListaEstatica;
+import utils.commons.Comparador;
+
 public class PancakeSort {
 
+    private static int indiceMayor(ListaEstatica lista){
+        int indiceMayor = 0;
+        for(int cadaIndice = 0; cadaIndice <= lista.getTope(); cadaIndice++){
+            if((int) Comparador.comparar(lista.obtener(cadaIndice), lista.obtener(indiceMayor)) > 0){
+                indiceMayor = cadaIndice;
+            }
+        }
+        return indiceMayor;
+    }
+
+    private static void cambio(ListaEstatica lista, int indiceMayor){
+        for(int cadaIndice = 0; cadaIndice < indiceMayor; cadaIndice++){
+            Object temporal = lista.obtener(cadaIndice);
+            lista.cambiar(cadaIndice,lista.obtener(indiceMayor));
+            lista.cambiar(indiceMayor, temporal);
+            indiceMayor --;
+        }
+    }
+
+    public static void sort(ListaEstatica lista){
+        for(int tamanio = lista.getTope(); tamanio >= 1; tamanio--){
+            int indiceMayor = indiceMayor(lista);
+            if(indiceMayor != tamanio-1){
+                cambio(lista, indiceMayor);
+                cambio(lista, tamanio-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ListaEstatica lista = new ListaEstatica(7);
+        lista.agregar(23);
+        lista.agregar(10);
+        lista.agregar(20);
+        lista.agregar(11);
+        lista.agregar(12);
+        lista.agregar(6);
+        lista.agregar(7);
+
+        lista.imprimir();
+        SalidaPorDefecto.terminal("\n");
+        sort(lista);
+        lista.imprimir();
+    }
 }
--- src/utils/commons/Comparador.java
+++ src/utils/commons/Comparador.java
@@ -3,7 +3,7 @@ package utils.commons;
 public class Comparador {
 
     public static Object comparar(Object objeto1, Object objeto2) {
-        if (objeto1 instanceof Number && objeto2 instanceof Number) {
+        if (objeto1 instanceof Number && objeto2 instanceof Number ) {
             if(((Number) objeto1).doubleValue() > ((Number) objeto2).doubleValue()) {
                 return 1;
             } else if (((Number) objeto1).doubleValue() < ((Number) objeto2).doubleValue()){
