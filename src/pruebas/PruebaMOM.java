263e8bddc125db0ec657609417ba45bd2edbd4b5
aratt <aratt.jzl@gmail.com>
2023-05-29 16:10:57 -0600

    se agregaron cosas al trabajo final

--- src/pruebas/PruebaMOM.java
+++ src/pruebas/PruebaMOM.java
@@ -7,38 +7,38 @@ import vistas.VentanaPrincipal;
 public class PruebaMOM {
     public static void main(String[] args) {
         ModeloOcultoMarkov mom = new ModeloOcultoMarkov(4);
-        mom.agregarEstado("Exploracion", 0.4);
+        mom.agregarEstado("Explorar", 0.4);
         mom.agregarEstado("Compra", 0.3);
         mom.agregarEstado("Retorno", 0.2);
         mom.agregarEstado("Abandono", 0.1);
 
                 //TRANSICION//
-        mom.agregarProbabilidadTransicion("Exploracion", "Exploracion", 0.6);
-        mom.agregarProbabilidadTransicion("Exploracion", "Compra", 0.2);
-        mom.agregarProbabilidadTransicion("Exploracion", "Retorno", 0.1);
-        mom.agregarProbabilidadTransicion("Exploracion", "Abandono", 0.1);
+        mom.agregarProbabilidadTransicion("Explorar", "Explorar", 0.6);
+        mom.agregarProbabilidadTransicion("Explorar", "Compra", 0.2);
+        mom.agregarProbabilidadTransicion("Explorar", "Retorno", 0.1);
+        mom.agregarProbabilidadTransicion("Explorar", "Abandono", 0.1);
 
-        mom.agregarProbabilidadTransicion("Compra", "Exploracion", 0.1);
+        mom.agregarProbabilidadTransicion("Compra", "Explorar", 0.1);
         mom.agregarProbabilidadTransicion("Compra", "Compra", 0.5);
         mom.agregarProbabilidadTransicion("Compra", "Retorno", 0.3);
         mom.agregarProbabilidadTransicion("Compra", "Abandono", 0.1);
 
-        mom.agregarProbabilidadTransicion("Retorno", "Exploracion", 0.3);
+        mom.agregarProbabilidadTransicion("Retorno", "Explorar", 0.3);
         mom.agregarProbabilidadTransicion("Retorno","Compra",0.1);
         mom.agregarProbabilidadTransicion("Retorno","Retorno", 0.4);
         mom.agregarProbabilidadTransicion("Retorno", "Abandono", 0.2);
 
-        mom.agregarProbabilidadTransicion("Abandono", "Exploracion", 0.1);
+        mom.agregarProbabilidadTransicion("Abandono", "Explorar", 0.1);
         mom.agregarProbabilidadTransicion("Abandono", "Compra", 0.1);
         mom.agregarProbabilidadTransicion("Abandono", "Retorno", 0.1);
         mom.agregarProbabilidadTransicion("Abandono", "Abandono", 0.7);
 
                 //EMISION//
-        mom.agregarProbabilidadEmision("Exploracion", "Navegar", 0.6);
-        mom.agregarProbabilidadEmision("Exploracion", "Agregar al carrito", 0.1);
-        mom.agregarProbabilidadEmision("Exploracion", "Finalizar compra", 0.1);
-        mom.agregarProbabilidadEmision("Exploracion", "Solicitar devolucion", 0.1);
-        mom.agregarProbabilidadEmision("Exploracion", "Salir", 0.1);
+        mom.agregarProbabilidadEmision("Explorar", "Navegar", 0.6);
+        mom.agregarProbabilidadEmision("Explorar", "Agregar al carrito", 0.1);
+        mom.agregarProbabilidadEmision("Explorar", "Finalizar compra", 0.1);
+        mom.agregarProbabilidadEmision("Explorar", "Solicitar devolucion", 0.1);
+        mom.agregarProbabilidadEmision("Explorar", "Salir", 0.1);
 
         mom.agregarProbabilidadEmision("Compra","Navegar", 0.2);
         mom.agregarProbabilidadEmision("Compra", "Agregar al carrito", 0.4);
@@ -62,9 +62,9 @@ public class PruebaMOM {
 
         //SalidaPorDefecto.terminal(mom.probabilidadDeRealizarActividad("Abandono", "Salir") + "\n");
 
-        //SalidaPorDefecto.terminal(mom.probabilidadCiertoEstado("Exploracion", "Compra") + "\n");
+        //SalidaPorDefecto.terminal(mom.probabilidadCiertoEstado("Explorar", "Compra") + "\n");
 
-        //SalidaPorDefecto.terminal(mom.pobabilidadDeSecuenciaDeEstados("Abandono,Compra,Abandono,Retorno,Exploracion") + "\n");
+        //SalidaPorDefecto.terminal(mom.pobabilidadDeSecuenciaDeEstados("Abandono,Compra,Abandono,Retorno,Explorar") + "\n");
 
         VentanaPrincipal ventana = new VentanaPrincipal(mom);
     }
